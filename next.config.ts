d91b0ffce6d2fe5dfe51167e06e96afcea6ae6e5
Emily Souza <emilyb.souza03@gmail.com>
2025-02-02 21:18:15 -0400

    ajuste

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 const repositoryName = "myProfile";
 
 module.exports = {
+  output: "export",
   basePath: isProd ? `/${repositoryName}` : "",
   assetPrefix: isProd ? `/${repositoryName}/` : "",
   reactStrictMode: true,
